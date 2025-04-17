e3a5953c5e7fbff8aa262273dd15b3fde689b344
AldenHarcourt <aldenharcourt@gmail.com>
2025-04-17 16:41:18 +0000

    Made my website work at aldenharcourt.comgit config user.email aldenharcourt@gmail.com!

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.post('/submit-contact', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
